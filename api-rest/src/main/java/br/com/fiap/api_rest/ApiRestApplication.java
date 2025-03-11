cdb0ea3b0bce9327501459d8bafbfd0230e29656
Pedro-sugoy <pedrovisckyokoo@gmail.com>
2025-03-11 11:59:31 -0300

    Atualização dia 11-03-2025

--- api-rest/src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
+++ api-rest/src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
@@ -1,12 +1,17 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API de clientes", description = "Exemplo de API RESTFUL com Swagger", version = "0.0.0v1"))
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
+
 }
--- api-rest/src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
+++ api-rest/src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
@@ -6,6 +6,12 @@ import br.com.fiap.api_rest.model.Cliente;
 import br.com.fiap.api_rest.repository.ClienteRepository;
 import br.com.fiap.api_rest.service.ClienteService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -34,12 +40,26 @@ public class ClienteController {
     // create read update delete
     // post get put delete
 
+    @Operation(summary = "Cria um novo cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente cadastrado com sucesso ",
+                content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "400", description = "Atributos informados são inválidos",
+                content = @Content(schema = @Schema()))
+    })
     @PostMapping
-    public ResponseEntity<Cliente> createCliente(@RequestBody ClienteRequest cliente) {
+    public ResponseEntity<Cliente> createCliente(@Valid @RequestBody ClienteRequest cliente) {
         Cliente clienteSalvo = clienteRepository.save(clienteService.requestToCliente(cliente));
         return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Retorna uma lista de clientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente cadastrado com sucesso ",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "400", description = "Atributos informados são inválidos",
+                    content = @Content(schema = @Schema()))
+    })
     @GetMapping
     public ResponseEntity<Page<ClienteResponse>> readClientes(@RequestParam(defaultValue = "0") Integer page) {
         Pageable pageable = PageRequest
@@ -52,6 +72,13 @@ public class ClienteController {
 
     //PathVariable = parametro diretamente na url ex: /cliente/
     //RequestParam = paramtro como o qurey ex: /cliente/?id=1
+    @Operation(summary = "Retorna um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "404", description = "Nenhum cliente encontrado",
+                    content = @Content(schema = @Schema()))
+    })
     @GetMapping("/{id}")
     public ResponseEntity <ClienteResponse> readCliente(@PathVariable Long id) {
         Optional<Cliente> cliente = clienteRepository.findById(id);
@@ -61,20 +88,38 @@ public class ClienteController {
         return new ResponseEntity<>(clienteService.clienteToResponse(cliente.get(), false), HttpStatus.OK);
     }
 
+    @Operation(summary = "Atualiza um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente encontrado e atualizado com sucesso ",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))}),
+            @ApiResponse(responseCode = "400", description = "Nenum cliente encontrado para atualizar",
+                    content = @Content(schema = @Schema()))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente cliente){
         Optional<Cliente> clienteExistente = clienteRepository.findById(id);
         if (clienteExistente.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         cliente.setId(clienteExistente.get().getId());
         Cliente clienteAtualizado = clienteRepository.save(cliente);
         return new ResponseEntity<>(clienteAtualizado, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "exclui um cliente por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Cliente excluido com sucesso",
+                    content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "nenhum cliente encontrado",
+                    content = @Content(schema = @Schema()))
+    })
     @DeleteMapping("/{id}")
     public  ResponseEntity<Void> deleteCliente(@PathVariable Long id){
         clienteRepository.deleteById(id);
+        Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+        if (clienteExistente.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
